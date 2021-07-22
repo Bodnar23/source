7eb9ec5d05f8a9cf02293f41aecf9bbe72c47abd
Anastasia <brilliantstar94@gmail.com>
2021-07-22 20:39:23 +0300

    -ainit commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,11 @@ import tabs from './modules/tabs';
 import froms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
+    "use strict";  //включаем строгий режим внутри нашего файла
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms();
 });
 
